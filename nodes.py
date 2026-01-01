7298509bebb53dcb88b51a6edbd96ed64db15df8
lihaoyun6 <lihaoyun11@gmail.com>
2026-01-01 18:53:43 +0800

    add PromptEnhancerPreset node

--- nodes.py
+++ nodes.py
@@ -11,6 +11,7 @@ from PIL import Image, ImageDraw
 from scipy.ndimage import gaussian_filter
 from .support.cqdm import cqdm
 from .support.gguf_layers import get_layer_count
+from .support.prompt_enhancer_preset import *
 
 import folder_paths
 import comfy.model_management as mm
@@ -935,6 +936,61 @@ class remove_code_block:
         if len(output) == 1:
             return (output[0],)
         return (output,)
+
+class PromptEnhancerPreset:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "preset": (["Qwen-Image [EN]", "Qwen-Image [ZH]", "Qwen-Image 2512 [EN]", "Qwen-Image 2512 [ZH]", "Qwen-Image-Edit", "Qwen-Image-Edit 2509", "Qwen-Image-Edit 2511", "Z-Image Turbo", "Flux.2 T2I", "Flux.2 I2I", "Wan T2V [EN]", "Wan T2V [ZH]", "Wan I2V [EN]", "Wan I2V [ZH]", "Wan I2V Full-Auto [EN]", "Wan I2V Full-Auto [ZH]", "Wan FLF2V [EN]", "Wan FLF2V [ZH]"], )
+            }
+        }
+    
+    RETURN_TYPES = ("STRING",)
+    RETURN_NAMES = ("system_prompt",)
+    FUNCTION = "main"
+    CATEGORY = "llama-cpp-vlm"
+    
+    def main(self, preset):
+        match preset:
+            case "Qwen-Image [EN]":
+                return (QWEN_IMAGE_EN,)
+            case "Qwen-Image [ZH]":
+                return (QWEN_IMAGE_ZH,)
+            case "Qwen-Image 2512 [EN]":
+                return (QWEN_IMAGE_2512_EN,)
+            case "Qwen-Image 2512 [ZH]":
+                return (QWEN_IMAGE_2512_ZH,)
+            case "Qwen-Image-Edit":
+                return (QWEN_IMAGE_EDIT,)
+            case "Qwen-Image-Edit 2509":
+                return (QWEN_IMAGE_EDIT_2509,)
+            case "Qwen-Image-Edit 2511":
+                return (QWEN_IMAGE_EDIT_2511,)
+            case "Z-Image Turbo":
+                return (ZIMAGE_TURBO,)
+            case "Flux.2 T2I":
+                return (FLUX2_T2I,)
+            case "Flux.2 I2I":
+                return (FLUX2_I2I,)
+            case "Wan T2V [EN]":
+                return (WAN_T2V_EN,)
+            case "Wan T2V [ZH]":
+                return (WAN_T2V_ZH,)
+            case "Wan I2V [EN]":
+                return (WAN_I2V_EN,)
+            case "Wan I2V [ZH]":
+                return (WAN_I2V_ZH,)
+            case "Wan I2V Full-Auto [EN]":
+                return (WAN_I2V_EMPTY_EN,)
+            case "Wan I2V Full-Auto [ZH]":
+                return (WAN_I2V_EMPTY_ZH,)
+            case "Wan FLF2V [EN]":
+                return (WAN_FLF2V_EN,)
+            case "Wan FLF2V [ZH]":
+                return (WAN_FLF2V_ZH,)
+            case _:
+                raise ValueError(f'Unknow preset: "{preset}"')
         
 NODE_CLASS_MAPPINGS = {
     "llama_cpp_model_loader": llama_cpp_model_loader,
@@ -948,6 +1004,7 @@ NODE_CLASS_MAPPINGS = {
     "bbox_to_mask": bbox_to_mask,
     "bboxes_to_bbox": bboxes_to_bbox,
     "remove_code_block": remove_code_block,
+    "PromptEnhancerPreset": PromptEnhancerPreset,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
@@ -962,4 +1019,5 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "bbox_to_mask": "BBoxes to MASK",
     "bboxes_to_bbox": "BBoxes to BBox",
     "remove_code_block": "Unpack Code Block",
+    "PromptEnhancerPreset": "Prompt Enhancer Preset",
 }
